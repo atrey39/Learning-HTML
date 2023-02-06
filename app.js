99e4a46b55ab16b5f0d3fe068e5b91d9cf958ae3
Anant <atrey39@yahoo.com>
2023-02-05 19:56:35 -0500

    new work added

--- app.js
+++ app.js
@@ -4,6 +4,52 @@ let count = 0;
     count = count +1
     console.log (count)
 } */
-for (let i = 0; i < 100; i++){
-    console.log (i+1)
+/*for (let i = 1; i <21; i++){
+    if(i % 3 == 0 && i % 5 == 0){
+        console.log("Frontend Simplified")
+    }
+    else if(i % 3 == 0){
+        console.log("Frontend")
+    }
+    else if (i % 5 == 0 ){
+        console.log("Simplified")
+    }
+    else console.log(i)
 }
+let String = "Frontend Simplified"
+
+for (i = 0; i <= String.length; i++)
+{
+    console.log(String[i])
+}
+
+function convertCelsiusToFahrenheit(currentDegrees)
+{
+ return currentDegrees * 1.8 + 32;
+}
+
+console.log(convertCelsiusToFahrenheit(5));
+
+let grades = ["A+", "A", "FAIL"]
+
+let ExceledStudents = grades.filter(element => element != "FAIL")
+console.log(ExceledStudents)
+
+let moreGrades = ["FAIL", "FAIL", "B"]
+
+let mediocreStudents = moreGrades.filter(element => element != "FAIL")
+console.log(mediocreStudents)
+
+let grades = ["A+", "A", "FAIL"]
+let PassedStudents = []
+for (i = 0; i < grades.length; i++)
+{
+    if (grades[i] != "FAIL"){ 
+        PassedStudents.push(grades[i])
+    }
+}
+console.log(PassedStudents)*/
+
+let dollars = [1, 5, 10, 3]
+let cents = dollars.map(element => element *100)
+console.log(cents)
