bd50de963ff244fc9496dcc71f41ddc45a945647
Anant <atrey39@yahoo.com>
2023-02-08 19:11:23 -0500

    new work added

--- app.js
+++ app.js
@@ -50,6 +50,15 @@ for (i = 0; i < grades.length; i++)
 }
 console.log(PassedStudents)*/
 
+/*
 let dollars = [1, 5, 10, 3]
 let cents = dollars.map(element => element *100)
 console.log(cents)
+
+let cents = []
+for ( i = 0; i < dollars.length; i++)
+{
+    
+    cents.push(dollars[i]*100)
+}
+console.log(cents)*/
